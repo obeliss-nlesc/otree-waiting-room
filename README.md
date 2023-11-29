f991f43ac9579c265750942cbb4b6bb84c6dc576
Reggie Cushing <r.cushing@esciencecenter.nl>
2023-11-29 11:34:46 +0100

    updating documentation

--- README.md
+++ README.md
@@ -5,10 +5,40 @@ Run Redis server:
 ```
 docker run -d Redis
 ```
+
+## Install dependencies
+To install package dependencies
+```
+npm Install
+```
+
 ## Setup .env
-If using docker development environment then run 
+If using Docker ( [Docker setup](https://github.com/obeliss-nlesc/otree-docker) ) development environment then run 
 ```
 node set_env.js > .env
 ```
 to set the .env file. The script will scan the local docker images to get the IPs of the containers.
+
+## Generate public and private keys
+To generate keys for token encoding and verification, first generate the private key
+```
+openssl genpkey -algorithm RSA -out private-key.pem
+```
+then generate the public key
+```
+openssl rsa -pubout -in private-key.pem -out public-key.pem
+```
+
+## Start waiting room
+To start server on a default port 8060 localhost
+```
+npm run dev
+```
+Three test user URLs are printed on the command line that can be used to test the server.
+
+## API documentation
+API endpoints are documented using Postman and can be found [here](https://documenter.getpostman.com/view/1612141/2s9YeG7Bqm).
+
+
+
 
--- encode-token.js
+++ encode-token.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const commandLineArgs = process.argv.slice(2);
 const privateKeyPath = './private-key.pem';
 const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
-//console.log(commandLineArgs)
 
 // Replace these values with your actual data and private key file path
 const userTemplate = {
   userId: '',
+  experimentId: 'guess_two_thirds',
   headerText: 'In het gevangen dillema spel speelt u samen met 1 ander panellid. U wordt steeds gevraagd om een keuze te maken tussen samenwerken of tegenwerken. In totaal speelt u 10 rondes. Tijdens deze rondes veranderen er soms dingen, dit krijgt u steeds aan het begin van de ronde te zien.',
   meedoenValue: '€0,25',
   afrondenValue: '€2',
@@ -27,11 +26,10 @@ const users = ['user001', 'user002', 'user003']
 const userTokens = users.map(u => {
   const userData = clone(userTemplate)
   userData.userId = u
-  console.log(userData)
-  return jwt.sign(userData, privateKey, {algorithm: 'RS256', expiresIn: '1h'})
+  return jwt.sign(userData, privateKey, {algorithm: 'RS256', expiresIn: '12h'})
 })
 
 userTokens.forEach(t => {
-  console.log(t)
+  console.log(`http://localhost:8060/room/guess_two_thirds?token=${t}`)
   console.log()
 })
