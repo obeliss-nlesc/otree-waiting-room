9bc9657b7084c97a590502309dbce779ae07a34c
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-06-12 14:45:21 +0200

    update test script

--- server_test/test_user_flow.js
+++ server_test/test_user_flow.js
@@ -1,5 +1,6 @@
 const VirtualUser = require("./virtual-user-ws")
 const fs = require("fs")
+const http = require('http');
 
 function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min) + min)
@@ -10,17 +11,48 @@ const experimentId = "public_goods_game"
 const url = "http://localhost:8060"
 const virtUsers = {}
 
-for (let i = 0; i < maxUsers; i++) {
-  const id = 1000 + i
-  //const id = randomBetween(1, 9999999)
-  vu = new VirtualUser(id, experimentId, url)
-  virtUsers[id] = vu
+function runTest() {
+  for (let i = 0; i < maxUsers; i++) {
+    const id = 1000 + i
+    //const id = randomBetween(1, 9999999)
+    vu = new VirtualUser(id, experimentId, url)
+    virtUsers[id] = vu
+  }
+
+  Object.values(virtUsers).forEach((vu) => {
+    vu.connect().then(() => {
+      // true: random agree or not agree
+      // false: always agree
+      vu.attemptQueueFlow(true)
+    })
+  })
 }
 
-Object.values(virtUsers).forEach((vu) => {
-  vu.connect().then(() => {
-    // true: random agree or not agree
-    // false: always agree
-    vu.attemptQueueFlow(true)
-  })
-})
+
+const options = {
+  hostname: 'localhost',
+  port: 8060,
+  path: '/',
+  method: 'GET'
+};
+
+const req = http.request(options, (res) => {
+  if (res.statusCode > 0) {
+    console.log('Server is up and running.');
+    runTest()
+  } 
+
+  res.on('data', (chunk) => {
+  });
+
+  res.on('end', () => {
+  });
+});
+
+req.on('error', (e) => {
+  console.log('Server is down or not reachable.');
+  console.log('Start server with command e.g. "npm run dev".')
+});
+
+req.end();
+
