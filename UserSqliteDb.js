25465bec8a76c7426ca143429a272613d8e7cb24
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-06-28 15:52:33 +0200

    fixing test

--- UserSqliteDb.js
+++ UserSqliteDb.js
@@ -17,29 +17,33 @@ class UserDb extends Map {
     })
   }
   load() {
-    const query = `SELECT * FROM users`
-    this.db.all(query, (err, rows) => {
-      if (err) {
-        console.error(err)
-      } else {
-        rows
-          .map((r) => {
-            return JSON.parse(r.jsonObj)
-          })
-          .forEach((u) => {
-            console.log(u)
-            const user = new User(u.userId, u.experimentId)
-            user.redirectedUrl = u.redirectedUrl
-            user.experimentUrl = u.experimentUrl
-            user.groupId = u.groupId
-            user.oTreeId = u.oTreeId
-            user.tokenParams = u.tokenParams
-            user.state = u.redirectedUrl ? u.state : user.state
-            const compoundKey = `${user.userId}:${user.experimentId}`
-            this.set(compoundKey, user)
-          })
-      }
-    })
+    return new Promise((resolve, reject) => {
+      const query = `SELECT * FROM users`
+      this.db.all(query, (err, rows) => {
+        if (err) {
+          console.error(err)
+          reject(err)
+        } else {
+          rows
+            .map((r) => {
+              return JSON.parse(r.jsonObj)
+            })
+            .forEach((u) => {
+              console.log(u)
+              const user = new User(u.userId, u.experimentId)
+              user.redirectedUrl = u.redirectedUrl
+              user.experimentUrl = u.experimentUrl
+              user.groupId = u.groupId
+              user.oTreeId = u.oTreeId
+              user.tokenParams = u.tokenParams
+              user.state = u.redirectedUrl ? u.state : user.state
+              const compoundKey = `${user.userId}:${user.experimentId}`
+              this.set(compoundKey, user)
+            })
+          resolve(this)
+        }
+      })
+    })//Promise
   }
   find(userId) {
     return Array.from(this.values()).filter((u) => {
