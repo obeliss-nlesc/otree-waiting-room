b071599f2482efa68a6720de58075d445c056655
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-06-28 18:39:38 +0200

    adding interface class UserMap

---
+++ UserMap.js
@@ -0,0 +1,65 @@
+const User = require("./user.js")
+
+class UserMap extends Map {
+  constructor(file) {
+    super()
+  }
+  
+  load() {
+    throw new Error('Method not implemented.');
+  }
+
+  find(userId) {
+    return Array.from(this.values()).filter((u) => {
+      return u.userId == userId
+    })
+  }
+
+  getUsersInSession(session) {
+    return Array.from(this.values()).filter((u) => {
+      if (!u.server) {
+        return false
+      }
+      const userSession = u.server.split("#")[1]
+      return userSession == session
+    })
+  }
+
+  getUsedUrls() {
+    return Array.from(this.values())
+      .filter((u) => {
+        return u.experimentUrl
+      })
+      .map((u) => {
+        return u.experimentUrl
+      })
+  }
+
+  dump() {
+    const data = []
+    this.forEach((v, k) => {
+      data.push(v.serialize())
+    })
+
+    return JSON.stringify(data)
+  }
+
+  upsert(user) {
+    throw new Error('Method not implemented.');
+  }
+
+  save(user) {
+    throw new Error('Method not implemented.');
+  }
+
+  saveAll() {
+    throw new Error('Method not implemented.');
+  }
+
+  forceSave() {
+    throw new Error('Method not implemented.');
+  }
+}
+
+module.exports = UserMap
+
--- UserSqliteDb.js
+++ UserSqliteDb.js
@@ -1,8 +1,9 @@
 const sqlite3 = require("sqlite3").verbose()
 // const db = new sqlite3.Database(':memory:');
 const User = require("./user.js")
+const UserMap = require("./UserMap.js")
 
-class UserDb extends Map {
+class UserDb extends UserMap {
   constructor(file) {
     super()
     this.writeCounter = 0
@@ -29,7 +30,6 @@ class UserDb extends Map {
               return JSON.parse(r.jsonObj)
             })
             .forEach((u) => {
-              console.log(u)
               const user = new User(u.userId, u.experimentId)
               user.redirectedUrl = u.redirectedUrl
               user.experimentUrl = u.experimentUrl
@@ -45,37 +45,6 @@ class UserDb extends Map {
         }
       })
     }) //Promise
-  }
-  find(userId) {
-    return Array.from(this.values()).filter((u) => {
-      return u.userId == userId
-    })
-  }
-  getUsersInSession(session) {
-    return Array.from(this.values()).filter((u) => {
-      if (!u.server) {
-        return false
-      }
-      const userSession = u.server.split("#")[1]
-      return userSession == session
-    })
-  }
-  getUsedUrls() {
-    return Array.from(this.values())
-      .filter((u) => {
-        return u.experimentUrl
-      })
-      .map((u) => {
-        return u.experimentUrl
-      })
-  }
-  dump() {
-    const data = []
-    this.forEach((v, k) => {
-      data.push(v.serialize())
-    })
-
-    return JSON.stringify(data)
   }
   upsert(user) {
     return new Promise((resolve, reject) => {
@@ -139,41 +108,3 @@ class UserDb extends Map {
 
 module.exports = UserDb
 
-// db.serialize(() => {
-//   db.run(`
-//     CREATE TABLE IF NOT EXISTS users (
-//       userId TEXT PRIMARY KEY,
-//       jsonObj TEXT
-//     )
-//   `);
-// });
-//
-// db.saveUser = function(user) {
-//     return new Promise((resolve, reject) => {
-//       const query = `INSERT INTO users (userId, jsonObj) VALUES (?, ?)`;
-//       db.run(query, [user.userId, JSON.stringify(user.serialize())], function (err) {
-//         if (err) {
-//           reject(err);
-//         } else {
-//           resolve();
-//         }
-//       });
-//     });
-// }
-//
-// db.getTables = function() {
-//   return new Promise((resolve, reject) => {
-//     const query = `SELECT name FROM sqlite_master WHERE type='table'`;
-//     db.all(query, (err, rows) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(rows.map(row => row.name));
-//       }
-//     });
-//   });
-// };
-//
-//
-// module.exports = db;
-//
