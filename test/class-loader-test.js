60d1ab85414e8682c3e4e9123819075b3fe20429
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-07-02 11:01:52 +0200

    fix test

--- test/class-loader-test.js
+++ test/class-loader-test.js
@@ -1,6 +1,6 @@
 const test = require("node:test")
 const assert = require("node:assert")
-const ClassLoader = require("../class_loader.js")
+const ClassLoader = require("../class-loader.js")
 
 test("test private constructor ClassLoader", async () => {
   assert.throws(() => {
