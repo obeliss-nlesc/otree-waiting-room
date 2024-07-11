a572bc478e48813c7b0e5b331037a7b96c8c35bf
Reggie Cushing <r.cushing@esciencecenter.nl>
2024-07-11 14:02:19 +0200

    refactor round robin function

--- schedulers/gat-scheduler.js
+++ schedulers/gat-scheduler.js
@@ -78,28 +78,51 @@ class GatScheduler {
     const keys = Object.keys(experiments)
     const values = Object.values(experiments)
     // console.log(keys)
-
-    for (let i = 0; i < keys.length; i++) {
-      this.currentIndex = (this.currentIndex + 1) % keys.length
-      const serverIp = keys[this.currentIndex]
-      const serverUrls = values[this.currentIndex]
-      if (serverUrls.length < this.min) {
-        continue
-      }
-
-      let unusedUrlsCount = 0
-      for (let serverUrl of serverUrls) {
-        if (!usedUrls.has(serverUrl)) {
-          unusedUrlsCount += 1
+    if (this.useRoundRobin) {
+      // Use Round Robin load balancing. This is done over sessions not server ips
+      for (let i = 0; i < keys.length; i++) {
+        this.currentIndex = (this.currentIndex + 1) % keys.length
+        const serverIp = keys[this.currentIndex]
+        const serverUrls = values[this.currentIndex]
+        if (serverUrls.length < this.min) {
+          continue
         }
+
+        let unusedUrlsCount = 0
+        for (let serverUrl of serverUrls) {
+          if (!usedUrls.has(serverUrl)) {
+            unusedUrlsCount += 1
+          }
+        }
+        if (unusedUrlsCount < this.min) {
+          continue
+        }
+        // console.log(`index ${this.currentIndex} server ${serverIp}`)
+        return [serverIp, serverUrls]
       }
-      if (unusedUrlsCount < this.min) {
-        continue
-      }
-      // console.log(`index ${this.currentIndex} server ${serverIp}`)
-      return [serverIp, serverUrls]
-    }
-    return [null, null]
+      return [null, null]
+    } else {
+      // Fill up first server first (Spillover) load balancer
+      for (const [serverIp, serverUrls] of Object.entries(
+        experiments[this.experimentName].servers,
+      )) {
+        if (serverUrls.length < this.min) {
+          continue
+        }
+
+        let unusedUrlsCount = 0
+        for (let serverUrl of serverUrls) {
+          if (!usedUrls.has(serverUrl)) {
+            unusedUrlsCount += 1
+          }
+        }
+        if (unusedUrlsCount < this.min) {
+          continue
+        }
+        return [serverIp, serverUrls]
+      } // For
+      return [null, null]
+    } // Else
   }
 
   checkConditionAndReturnUsers(experiments, usedUrls) {
@@ -123,80 +146,34 @@ class GatScheduler {
       return falseCondition
     }
 
-    if (this.useRoundRobin) {
-      // Use Round Robin load balancing. This is done over sessions not server ips
-
-      const [serverIp, serverUrls] = this.#getNextServer(
-        experiments[this.experimentName].servers,
-        usedUrls,
-      )
-      if (!serverIp) {
-        return falseCondition
-      }
-
-      const users = this.queue.pop(this.min)
-      for (const user of users) {
-        while (true) {
-          const url = serverUrls.pop()
-          if (!usedUrls.has(url)) {
-            user.redirectedUrl = url
-            user.changeState("waitAgreement")
-            usedUrls.add(url)
-            break
-          }
-          serverUrls.unshift(url)
-        }
-      }
-      // this.queue.unlock()
-      return {
-        condition: true,
-        users: users,
-        waitForCount: 0,
-        server: serverIp,
-      }
-    } else {
-      // Fill up first server first (Spillover) load balancer
-      for (const [serverIp, serverUrls] of Object.entries(
-        experiments[this.experimentName].servers,
-      )) {
-        if (serverUrls.length < this.min) {
-          continue
-        }
-
-        let unusedUrlsCount = 0
-        for (let serverUrl of serverUrls) {
-          if (!usedUrls.has(serverUrl)) {
-            unusedUrlsCount += 1
-          }
-        }
-        if (unusedUrlsCount < this.min) {
-          continue
-        }
-
-        const users = this.queue.pop(this.min)
-        for (const user of users) {
-          while (true) {
-            const url = serverUrls.pop()
-            if (!usedUrls.has(url)) {
-              user.redirectedUrl = url
-              user.changeState("waitAgreement")
-              usedUrls.add(url)
-              break
-            }
-            serverUrls.unshift(url)
-          }
-        }
-        // this.queue.unlock()
-        return {
-          condition: true,
-          users: users,
-          waitForCount: 0,
-          server: serverIp,
-        }
-      }
-      // this.queue.unlock()
+    const [serverIp, serverUrls] = this.#getNextServer(
+      experiments[this.experimentName].servers,
+      usedUrls,
+    )
+    if (!serverIp) {
       return falseCondition
-    } // Else
+    }
+
+    const users = this.queue.pop(this.min)
+    for (const user of users) {
+      while (true) {
+        const url = serverUrls.pop()
+        if (!usedUrls.has(url)) {
+          user.redirectedUrl = url
+          user.changeState("waitAgreement")
+          usedUrls.add(url)
+          break
+        }
+        serverUrls.unshift(url)
+      }
+    }
+    // this.queue.unlock()
+    return {
+      condition: true,
+      users: users,
+      waitForCount: 0,
+      server: serverIp,
+    }
   }
 } // Class
 
